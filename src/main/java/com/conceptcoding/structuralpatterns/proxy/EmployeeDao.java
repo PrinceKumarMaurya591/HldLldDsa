11e80d9acc263fecc6e22c78f5868ee17468647d
Shrayansh Jain <shrayanshjain@Shrayanshs-MacBook-Pro.local>
2025-11-14 15:07:54 +0530

    proxy code updated

--- src/main/java/com/conceptcoding/structuralpatterns/proxy/EmployeeDao.java
+++ src/main/java/com/conceptcoding/structuralpatterns/proxy/EmployeeDao.java
@@ -2,14 +2,6 @@ package com.conceptcoding.structuralpatterns.proxy;
 
 // Subject interface - common interface for RealSubject and Proxy
 public interface EmployeeDao {
-
-    void getEmployeeInfo();
-
-    void createEmployee();
-
-    void deleteEmployee();
-
-    void updateSalary(double newSalary);
-
-    void displayEmployeeDetails();
+    void getEmployeeInfo(int empID);
+    void createEmployee(EmployeeDo obj);
 }
